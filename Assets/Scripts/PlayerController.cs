0e2296dd11f28d3994a541626fa833f83654bfa2
Sávio Luiz <savioluiz546@gmail.com>
2024-11-12 23:37:31 -0300

    Animation-walk

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D _playerRigidibody2D;
+    private Rigidbody2D playerRig;
+    private Animator anima;
     
     //Controlar velocidade do player
     public float _playerSpeed;
@@ -15,17 +17,60 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _playerRigidibody2D = GetComponent<Rigidbody2D>();
+        playerRig = GetComponent<Rigidbody2D>();
+        anima = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        _playerDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        MovePlayer();
+       
     }
-    void FixedUpdate()
+    void MovePlayer()
     {
-        _playerRigidibody2D.MovePosition(_playerRigidibody2D.position + _playerDirection * _playerSpeed * Time.fixedDeltaTime);
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+        transform.position += movement * _playerSpeed * Time.deltaTime;
+        //Animacao no EIXO "X"
+        if (Input.GetAxis("Horizontal") > 0f)
+        {
+            anima.SetBool("walkRight", true);
+            //anima.SetBool("walkLeft", false);
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+        if (Input.GetAxis("Horizontal") < 0f)
+        {
+            anima.SetBool("walkLeft", true);
+            //anima.SetBool("walkRight", false);
+            transform.eulerAngles = new Vector3(0f, +180f, 0f);
+        }
+        if (Input.GetAxis("Horizontal") == 0f)
+        {
+            anima.SetBool("walkLeft", false);
+            anima.SetBool("walkRight", false);
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+        
+        // Animacao no EIXO "Y"
+        if (Input.GetAxis("Vertical") > 0f)
+        {
+            anima.SetBool("walkUp", true);
+            //anima.SetBool("walkLeft", false);
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+        if (Input.GetAxis("Vertical") < 0f)
+        {
+            anima.SetBool("walkDown", true);
+            //anima.SetBool("walkRight", false);
+            transform.eulerAngles = new Vector3(0f, +180f, 0f);
+        }
+        if (Input.GetAxis("Vertical") == 0f)
+        {
+            anima.SetBool("walkUp", false);
+            anima.SetBool("walkDown", false);
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+        
     }
     
 }
