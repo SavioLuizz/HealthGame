54c0c7a66bd93b5e61b252b0b3afd4b39b330bcc
Sávio Luiz <savioluiz546@gmail.com>
2024-11-17 20:07:06 -0300

    add animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D playerRig;
     private Animator anima;
+    private Vector2 movement;
+    public float speed = 5f;
     
     //Controlar velocidade do player
     public float _playerSpeed;
@@ -24,53 +26,77 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Captura das teclas
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        // Zera o movimento diagonal
+        if (horizontal != 0) vertical = 0;
+
+        // Atualiza o movimento
+        movement = new Vector2(horizontal, vertical).normalized;
+
+        // Atualiza o Animator
+        //anima.SetFloat("Horizontal", movement.x);
+        //anima.SetFloat("Vertical", movement.y);
+        //anima.SetFloat("Speed", movement.sqrMagnitude);
         MovePlayer();
        
     }
+    void FixedUpdate()
+    {
+        // Movimenta o personagem
+       // transform.Translate(movement * speed * Time.fixedDeltaTime);
+    }
     void MovePlayer()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+        // Captura os valores dos eixos
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // Zera a diagonal: prioriza o movimento horizontal
+        if (horizontal != 0) vertical = 0;
+
+        // Aplica o movimento ao personagem
+        Vector3 movement = new Vector3(horizontal, vertical, 0f);
         transform.position += movement * _playerSpeed * Time.deltaTime;
-        //Animacao no EIXO "X"
-        if (Input.GetAxis("Horizontal") > 0f)
+
+        // Animação no eixo "X"
+        if (horizontal > 0f)
         {
             anima.SetBool("walkRight", true);
-            //anima.SetBool("walkLeft", false);
-            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+            anima.SetBool("walkLeft", false);
+            //transform.eulerAngles = new Vector3(0f, 0f, 0f);
         }
-        if (Input.GetAxis("Horizontal") < 0f)
+        else if (horizontal < 0f)
         {
             anima.SetBool("walkLeft", true);
-            //anima.SetBool("walkRight", false);
-            transform.eulerAngles = new Vector3(0f, +180f, 0f);
-        }
-        if (Input.GetAxis("Horizontal") == 0f)
-        {
-            anima.SetBool("walkLeft", false);
             anima.SetBool("walkRight", false);
-            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+            //transform.eulerAngles = new Vector3(0f, 180f, 0f);
         }
-        
-        // Animacao no EIXO "Y"
-        if (Input.GetAxis("Vertical") > 0f)
+        else
+        {
+            anima.SetBool("walkRight", false);
+            anima.SetBool("walkLeft", false);
+        }
+
+        // Animação no eixo "Y"
+        if (vertical > 0f)
         {
             anima.SetBool("walkUp", true);
-            //anima.SetBool("walkLeft", false);
-            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+            anima.SetBool("walkDown", false);
         }
-        if (Input.GetAxis("Vertical") < 0f)
+        else if (vertical < 0f)
         {
             anima.SetBool("walkDown", true);
-            //anima.SetBool("walkRight", false);
-            transform.eulerAngles = new Vector3(0f, +180f, 0f);
+            anima.SetBool("walkUp", false);
         }
-        if (Input.GetAxis("Vertical") == 0f)
+        else
         {
             anima.SetBool("walkUp", false);
             anima.SetBool("walkDown", false);
-            transform.eulerAngles = new Vector3(0f, 0f, 0f);
         }
-        
     }
+
     
 }
